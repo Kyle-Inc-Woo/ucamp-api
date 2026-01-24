9047638e771d423d4c32fc114856561ab3673d62
user <dndlsckd0513@gmail.com>
2026-01-24 09:24:16 +0900

    spring boot kotlin project setup

---
+++ src/main/kotlin/com/ucamp/ucamp_api/UcampApiApplication.kt
@@ -0,0 +1,11 @@
+package com.ucamp.ucamp_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UcampApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<UcampApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/ucamp/ucamp_api/UcampApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ucamp.ucamp_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UcampApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
